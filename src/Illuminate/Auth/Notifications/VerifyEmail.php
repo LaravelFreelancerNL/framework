d535748ca4ec33b7446cebc7701903bbdf7b4efa
Bas <info@laravel-freelancer.nl>
2021-09-30 13:20:34 +0200

    Use routeKey instead of key

--- src/Illuminate/Auth/Notifications/VerifyEmail.php
+++ src/Illuminate/Auth/Notifications/VerifyEmail.php
@@ -84,7 +84,7 @@ class VerifyEmail extends Notification
             'verification.verify',
             Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60)),
             [
-                'id' => $notifiable->getKey(),
+                'id' => $notifiable->getRouteKey(),
                 'hash' => sha1($notifiable->getEmailForVerification()),
             ]
         );
